6e7d80217ae8fe201a7ef7d2f0cc564c50cde1f8
Shubham Chauhan <44020728+shubhiamchauhan@users.noreply.github.com>
2018-11-23 14:59:03 +0530

    modified test message of filter function.

--- test/filterTest.js
+++ test/filterTest.js
@@ -10,22 +10,25 @@ const isOdd = function(number) {
 }
 
 describe("filter", function() {
-  it("testing with single element array", function() {
+  it("with array of single element should return truthy value for given predicate", function() {
     assert.deepEqual(filter(isEven,[2]),[2]);
     assert.deepEqual(filter(isEven,[13]),[]);
     assert.deepEqual(filter(isOdd,[13]),[13]);
     assert.deepEqual(filter(isOdd,[2]),[]);
   });
 
-  it("testing with multiple elements array", function() {
+  it("with array of multiple element should return all truthy values of array for given predicate", function() {
     assert.deepEqual(filter(isEven,[1,2,3]),[2]);
     assert.deepEqual(filter(isOdd,[1,2,3]),[1,3]);
   });
 
-  it("testing with empty array and array contain only zero", function() {
+  it("with empty array should return empty array", function() {
     assert.deepEqual(filter(isOdd,[]),[]);
-    assert.deepEqual(filter(isOdd,[0]),[]);
     assert.deepEqual(filter(isEven,[]),[]);
+  });
+
+  it("with array of element 0 should return same array or empty array", function() {
+    assert.deepEqual(filter(isOdd,[0]),[]);
     assert.deepEqual(filter(isEven,[0]),[0]);
   });
 });
